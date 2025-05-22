707cfcbf1eaa7d4584fdbc42f4308565049332ec
Dayinta <dayintaajeng@students.unnes.ac.id>
2025-05-22 09:10:08 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_tittle="Dashbord App", layout="centered")
+st.sidebar.header("Dashbord")
+
 st.title("Hello, World!")
 st.write("Halloooo ini percobaan datmin hari iniiii :smile:")
