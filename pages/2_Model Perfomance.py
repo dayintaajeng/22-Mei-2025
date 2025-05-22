ac2c7d97259b368bb1c4530295bb85755bba86b7
Dayinta <dayintaajeng@students.unnes.ac.id>
2025-05-22 09:10:29 +0700

    Update 2_Model Perfomance.py

--- pages/2_Model Perfomance.py
+++ pages/2_Model Perfomance.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import panda as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accurary_score
 
-st.set_page_config(page_tittle="Prediction")
-st.title("Prediction")
-st.header("Prediction")
+st.set_page_config(page_tittle="Dashbord")
+st.title("Dashbord")
+st.sidebar.header("Dashbord")
